36d14ec7b2bf9ef7d915aa269d75b05bc35aad5e
Felipe Natan <39018332+fenatan@users.noreply.github.com>
2025-11-29 15:43:39 -0300

    fix backend

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -20,12 +20,26 @@ const config = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
   production: {
     dialect: 'mysql',
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
 
