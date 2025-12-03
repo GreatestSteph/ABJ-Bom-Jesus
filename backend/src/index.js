36a8ddc6c74f04f0049c33ba0fed043e4e0b00fd
Felipe Natan <39018332+fenatan@users.noreply.github.com>
2025-12-03 19:23:05 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,12 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
 
 app.use(routes);
 
